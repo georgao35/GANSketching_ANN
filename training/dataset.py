aef343585a10688f68caab72f792ef811ae53b0e
Zhang Wei <zhang-w19@mails.tsinghua.edu.cn>
2021-12-23 15:03:06 +0800

    Image normalize in dataloader

--- training/dataset.py
+++ training/dataset.py
@@ -9,23 +9,28 @@ import os
 import pickle
 import string
 import io
+
 # from torchvision import transforms
 
 
-IMAGE_EXTENSIONS = {'bmp', 'jpg', 'jpeg', 'pgm', 'png', 'ppm',
-                    'tif', 'tiff', 'webp'}
+IMAGE_EXTENSIONS = {"bmp", "jpg", "jpeg", "pgm", "png", "ppm", "tif", "tiff", "webp"}
+
+
 class ImagePathDataset(Dataset):
-    def __init__(self, path, image_mode='L', transform=None, max_images=None):
+    def __init__(self, path, image_mode="L", transform=None, max_images=None):
         super().__init__(batch_size=1)
         path = pathlib.Path(path)
-        files = sorted([file for ext in IMAGE_EXTENSIONS
-                       for file in path.glob('*.{}'.format(ext))])
+        files = sorted(
+            [file for ext in IMAGE_EXTENSIONS for file in path.glob("*.{}".format(ext))]
+        )
         if max_images is None:
             self.files = files
         elif max_images < len(files):
             self.files = random.sample(files, max_images)
         else:
-            print(f"max_images larger or equal to total number of files, use {len(files)} images instead.")
+            print(
+                f"max_images larger or equal to total number of files, use {len(files)} images instead."
+            )
             self.files = files
         self.transform = transform
         self.image_mode = image_mode
@@ -45,24 +50,34 @@ class ImagePathDataset(Dataset):
     def __len__(self):
         return len(self.files)
 
+
 class LmdbDataset(Dataset):
-    def __init__(self, path, image_mode='L', transform=None, max_images=None):
+    def __init__(self, path, image_mode="L", transform=None, max_images=None):
         # path = pathlib.Path(path)
         super().__init__(batch_size=1)
         self.transform = transform
         self.image_mode = image_mode
 
-        self.env = lmdb.open(path, max_readers=1, readonly=True, lock=False, readahead=False, meminit=False)
-        
-        with self.env.begin(write=False) as txn:
-            self.length = txn.stat()['entries']
+        self.env = lmdb.open(
+            path,
+            max_readers=1,
+            readonly=True,
+            lock=False,
+            readahead=False,
+            meminit=False,
+        )
 
-        cache_file = '_cache_' + ''.join(c for c in path if c in string.ascii_letters)
+        with self.env.begin(write=False) as txn:
+            self.length = txn.stat()["entries"]
+
+        cache_file = "_cache_" + "".join(c for c in path if c in string.ascii_letters)
         if os.path.isfile(cache_file):
             self.keys = pickle.load(open(cache_file, "rb"))
         else:
             with self.env.begin(write=False) as txn:
-                self.keys = [key for key in txn.cursor().iternext(keys=True, values=False)]
+                self.keys = [
+                    key for key in txn.cursor().iternext(keys=True, values=False)
+                ]
             pickle.dump(self.keys, open(cache_file, "wb"))
         self.set_attrs(total_len=self.length)
 
@@ -85,6 +100,7 @@ class LmdbDataset(Dataset):
     def __len__(self):
         return self.length
 
+
 def data_sampler(dataset, shuffle):
     if shuffle:
         return data.RandomSampler(dataset)
@@ -94,16 +110,19 @@ def data_sampler(dataset, shuffle):
 
 def create_dataloader(data_dir, size, batch, img_channel=3):
     mean, std = [0.5 for _ in range(img_channel)], [0.5 for _ in range(img_channel)]
-    transform = transforms.Compose([
+    transform = transforms.Compose(
+        [
             transforms.Resize(size),
             transforms.ToTensor(),
+            transforms.ImageNormalize(mean, std),
             # transforms.Normalize(mean, std, inplace=True),
-        ])
+        ]
+    )
 
     if img_channel == 1:
-        image_mode = 'L'
+        image_mode = "L"
     elif img_channel == 3:
-        image_mode = 'RGB'
+        image_mode = "RGB"
     else:
         raise ValueError("image channel should be 1 or 3, but got ", img_channel)
 
@@ -116,18 +135,22 @@ def create_dataloader(data_dir, size, batch, img_channel=3):
     # return loader, sampler
     return dataset, sampler
 
+
 def create_lmdb_dataloader(data_dir, size, batch, img_channel=3):
     mean, std = [0.5 for _ in range(img_channel)], [0.5 for _ in range(img_channel)]
-    transform = transforms.Compose([
+    transform = transforms.Compose(
+        [
             transforms.Resize(size),
             transforms.ToTensor(),
+            transforms.ImageNormalize(mean, std),
             # transforms.Normalize(mean, std, inplace=True),
-        ])
+        ]
+    )
 
     if img_channel == 1:
-        image_mode = 'L'
+        image_mode = "L"
     elif img_channel == 3:
-        image_mode = 'RGB'
+        image_mode = "RGB"
     else:
         raise ValueError("image channel should be 1 or 3, but got ", img_channel)
 
@@ -138,7 +161,7 @@ def create_lmdb_dataloader(data_dir, size, batch, img_channel=3):
     dataset.set_attrs(batch_size=batch, drop_last=True)
     # loader = data.DataLoader(dataset, batch_size=batch, sampler=sampler, drop_last=True)
     # return loader, sampler
-    return dataset, sampler    
+    return dataset, sampler
 
 
 def yield_data(loader, sampler, distributed=False):
